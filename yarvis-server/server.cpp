dc8575190578d5ae9424c4ac1d998f56f0910083
zhennTil <jspam@zhenntil.net>
2014-07-21 15:56:13 +0200

    Added Kiss FFT library, implemented flipping buffers and processing. Strange map initialization bug in BeatDetektor.

--- yarvis-server/server.cpp
+++ yarvis-server/server.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include "BeatDetektor.h"
 #include "portaudio.h"
+#include "kiss_fftr.h"
 
 using namespace std;
 
@@ -12,7 +13,10 @@ using namespace std;
 
 typedef struct
 {
-	
+	vector<float> buffer;
+	size_t buffer_size;
+	bool lock;
+	bool updated;
 } PaUserData;
 
 static int processAudioInput(
@@ -27,11 +31,17 @@ static int processAudioInput(
 	PaUserData *ud = (PaUserData*)userData;
 	float *in = (float*)inputBuffer;
 
+	// Replace buffer data
+	ud->lock = true;
+	ud->buffer.resize(framesPerBuffer);
+	ud->buffer_size = framesPerBuffer;
 	unsigned long i;
 	for ( i = 0; i<framesPerBuffer; i++ )
 	{
-		// TODO: FFT
+		ud->buffer[i] = in[i];
 	}
+	ud->lock = false;
+	ud->updated = true;
 
 	return 0;
 }
@@ -95,10 +105,53 @@ PaError openInputStream(PaStream** stream, const PaDeviceIndex& device, PaUserDa
 		data);
 }
 
+void processBuffer(const vector<float>& buffer, size_t buffer_size, kiss_fftr_cfg& fft_cfg, BeatDetektor& beat, float time)
+{
+	static size_t fft_size = 0;
+	static vector<kiss_fft_cpx> fft_out;
+	static vector<float> fft_sym;
+
+	// Ensure buffer is of even size
+	if (buffer_size % 2 > 0)
+	{
+		--buffer_size;
+	}
+
+	// TODO: Apply Hann window
+
+
+	// Allocate FFT memory if needed
+	if (fft_size != buffer_size)
+	{
+		if (fft_cfg != NULL) kiss_fftr_free(fft_cfg);
+		fft_cfg = kiss_fftr_alloc((int)buffer_size, 0, NULL, NULL);
+		fft_size = buffer_size;
+		fft_out.resize(fft_size / 2);
+		fft_sym.resize(fft_size);
+	}
+	
+	// Compute FFT
+	kiss_fftr(fft_cfg, buffer.data(), fft_out.data());
+
+	// Convert to symmetric real format for BeatDetektor
+	for (size_t i = 0; i < fft_size / 2; ++i)
+	{
+		fft_sym[i] = fft_out[i].r;
+		fft_sym[fft_size - 1 - i] = fft_out[i].r;
+	}
+
+	// Process with BeatDetektor
+	beat.process(time, fft_sym);
+}
+
 int main(int argc, char* argv[])
 {
-	// Initialize user data
+	// Initialize shared resources
+	kiss_fftr_cfg fft_cfg = NULL;
 	PaUserData data;
+	data.updated = false;
+	data.lock = false;
+	vector<float> backBuffer;
 
 	// Open audio input
 	PaError err = Pa_Initialize();
@@ -115,14 +168,30 @@ int main(int argc, char* argv[])
 
 	// Instantiate beat detektor
 	BeatDetektor bd(100.f, 199.f);
+	double startTime = Pa_GetStreamTime(stream);
 
 	// Check command input
 	char cmd = ' ';
 	cout << "Entering loop" << endl;
 	do
 	{
-		//this_thread::yield();
-		cin >> cmd;
+		//cin >> cmd;
+
+		// TODO: Place the buffer processing code in a separate thread, so we can allow the command input to block.
+		this_thread::yield();
+
+		// Has the interrupt updated its buffer?
+		if (data.updated)
+		{
+			// In case another interrupt has already started, wait for it to finish.
+			while (data.lock) this_thread::yield();
+			// Swap the buffers and clear the updated-flag.
+			data.buffer.swap(backBuffer);
+			data.updated = false;
+
+			// Finally, process the swapped-out buffer.
+			processBuffer(backBuffer, data.buffer_size, fft_cfg, bd, (float)(Pa_GetStreamTime(stream) - startTime));
+		}
 	} while (cmd != 'q');
 	cout << "Exiting loop" << endl;
 
@@ -132,6 +201,10 @@ int main(int argc, char* argv[])
 
 	err = Pa_Terminate();
 	HANDLE_PA_ERROR(err, "termination");
+
+	// Free the FFT memory, if allocated
+	if (fft_cfg != NULL)
+		kiss_fftr_free(fft_cfg);
 
 	return 0;
 }
